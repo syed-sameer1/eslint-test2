1190fe0d2f40fb3e7558ce1720448bc4d87c7cd2
syed-sameer1 <sameer.shah12570@gmail.com>
2019-11-03 18:44:39 +0500

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  var test = "sameer";
+  console.log(sameer);
+
   return (
     <div className="App">
       <header className="App-header">
